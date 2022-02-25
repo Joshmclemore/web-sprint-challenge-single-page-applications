ec78a07ea4653f08a676fbe0139dd19823f6d26e
Joshmclemore <josh_mclemore@yahoo.com>
2022-02-25 13:27:01 -0600

    Basic Routes are rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Switch, Route, Link } from 'react-router-dom';
+import OrderForm from "./Components/OrderForm";
 
 const App = () => {
+  console.log("hello world")
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/pizza" id="order-pizza">Order Pizza</Link>
+      </header>
+      <Switch>
+        <Route exact path="/">
+          <h1>Lambda Eats</h1>
+          <p>You can remove this code and create your own header</p>
+        </Route>
+        <Route path="/pizza">
+          <OrderForm/>
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
---
+++ src/Components/OrderForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderForm = () => {
+    return (
+    <form id="pizza-form">
+    <h1>Order Form</h1>
+    </form>
+    )
+}
+
+export default OrderForm
